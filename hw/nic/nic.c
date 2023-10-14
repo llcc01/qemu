760122d0a608c1730cda25cd9ecaafe61b85952c
llcc01 <2417224420@qq.com>
2023-10-14 22:48:06 +0800

    text pci rx & tx

--- hw/nic/nic.c
+++ hw/nic/nic.c
@@ -1,4 +1,5 @@
 #include "qemu/osdep.h"
+#include "qemu/qemu-print.h"
 #include "qemu/units.h"
 
 #include "hw/hw.h"
@@ -15,12 +16,15 @@
 #include "qom/object.h"
 
 #include "hw/nic/nic.h"
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 #define NIC_PCI_VENDOR_ID 0x11cc
 #define NIC_PCI_DEVICE_ID 0x1234
 
 #define NIC_MMIO_SIZE 0x00000100
-
 
 OBJECT_DECLARE_SIMPLE_TYPE(NICPangoState, NIC_PANGO)
 
@@ -28,7 +32,6 @@ struct NICPangoState {
   PCIDevice parent_obj;
 
   MemoryRegion mmio;
-  MemoryRegion msix;
 
   uint32_t ioaddr;
 
@@ -55,14 +58,165 @@ static const VMStateDescription nic_vmstate = {
         },
 };
 
+struct NICReg {
+  uint32_t tx_bd_head;
+  uint32_t tx_bd_tail;
+  dma_addr_t tx_bd_head_pa; // reserved
+  dma_addr_t tx_bd_tail_pa; // reserved
+  dma_addr_t tx_bd_pa;
+
+  uint32_t rx_bd_head; // reserved
+  uint32_t rx_bd_tail; // reserved
+  dma_addr_t rx_bd_head_pa;
+  dma_addr_t rx_bd_tail_pa;
+  dma_addr_t rx_bd_pa;
+
+  uint8_t intr;
+};
+
+struct TDArg {
+  PCIDevice *dev;
+  uint16_t dst;
+  uint16_t src;
+};
+
+static uint8_t nic_reg[NIC_MMIO_SIZE] = {0};
+
+static void *nic_copy_data(void *arg) {
+  PCIDevice *dev = ((struct TDArg *)arg)->dev;
+  uint16_t dst = ((struct TDArg *)arg)->dst;
+  uint16_t src = ((struct TDArg *)arg)->src;
+  struct NICReg *nic_reg_src =
+      (struct NICReg *)(nic_reg + NIC_MMIO_IF_REG_SIZE * src);
+  struct NICReg *nic_reg_dst =
+      (struct NICReg *)(nic_reg + NIC_MMIO_IF_REG_SIZE * dst);
+  struct NICBD nic_bd_src;
+  struct NICBD nic_bd_dst;
+  struct NICRxFrame nic_rx_frame;
+
+  free(arg);
+  while (nic_reg_src->tx_bd_tail != nic_reg_src->tx_bd_head) {
+    qemu_printf("nic_copy_data: tx_bd_tail = %u, tx_bd_head = %u\n",
+                nic_reg_src->tx_bd_tail, nic_reg_src->tx_bd_head);
+    if (nic_reg_src->tx_bd_head >= NIC_TX_RING_QUEUES) {
+      qemu_printf("nic_copy_data: tx_bd_head >= NIC_TX_RING_QUEUES\n");
+      goto err_frame;
+    }
+    pci_dma_read(dev,
+                 nic_reg_src->tx_bd_pa +
+                     sizeof(struct NICBD) * nic_reg_src->tx_bd_tail,
+                 &nic_bd_src, sizeof(struct NICBD));
+    pci_dma_read(dev,
+                 nic_reg_dst->rx_bd_pa +
+                     sizeof(struct NICBD) * nic_reg_dst->rx_bd_head,
+                 &nic_bd_dst, sizeof(struct NICBD));
+
+    qemu_printf("nic_copy_data: %u->%u %u\n", src, dst, nic_bd_src.len);
+    if (nic_bd_src.len > NIC_RX_PKT_SIZE) {
+      qemu_printf("nic_copy_data: nic_bd_src.len > NIC_RX_PKT_SIZE\n");
+      goto err_frame;
+    }
+
+    // copy data
+    nic_rx_frame.data_len = nic_bd_src.len;
+    pci_dma_read(dev, nic_bd_src.addr, &nic_rx_frame.data, nic_bd_src.len);
+
+    pci_dma_write(dev, nic_bd_dst.addr, &nic_rx_frame,
+                  nic_bd_src.len + sizeof(nic_rx_frame.data_len));
+
+  err_frame:
+    nic_reg_src->tx_bd_tail =
+        (nic_reg_src->tx_bd_tail + 1) % NIC_TX_RING_QUEUES;
+    nic_reg_dst->rx_bd_head =
+        (nic_reg_dst->rx_bd_head + 1) % NIC_RX_RING_QUEUES;
+    pci_dma_write(dev, nic_reg_dst->rx_bd_head_pa, &nic_reg_dst->rx_bd_head,
+                  sizeof(uint32_t));
+
+    // notify
+    if (msi_enabled(dev) && nic_reg_dst->intr) {
+      msi_notify(dev, 0);
+      qemu_printf("nic_copy_data: msi_notify\n");
+    }
+  }
+
+  return NULL;
+}
+
 static uint64_t nic_mmio_read(void *opaque, hwaddr addr, unsigned size) {
   //   NICPangoState *s = opaque;
+  qemu_printf("nic_mmio_read %lx(%u bytes)\n", addr, size);
+  if (addr + size > NIC_MMIO_SIZE) {
+    qemu_printf("nic_mmio_read: addr + size > NIC_MMIO_SIZE\n");
+    return 0;
+  }
+
+  switch (size) {
+  case 1:
+    return *(uint8_t *)(nic_reg + addr);
+  case 2:
+    return *(uint16_t *)(nic_reg + addr);
+  case 4:
+    return *(uint32_t *)(nic_reg + addr);
+  case 8:
+    return *(uint64_t *)(nic_reg + addr);
+  default:
+    break;
+  }
   return 0;
 }
 
 static void nic_mmio_write(void *opaque, hwaddr addr, uint64_t val,
                            unsigned size) {
   //   NICPangoState *s = opaque;
+  int res;
+  pthread_t thread;
+  struct TDArg *arg = malloc(sizeof(struct TDArg));
+  if (addr + size > NIC_MMIO_SIZE) {
+    qemu_printf("nic_mmio_write: addr + size > NIC_MMIO_SIZE\n");
+    return;
+  }
+
+  switch (size) {
+  case 1:
+    *(uint8_t *)(nic_reg + addr) = val;
+    break;
+  case 2:
+    *(uint16_t *)(nic_reg + addr) = val;
+    break;
+  case 4:
+    *(uint32_t *)(nic_reg + addr) = val;
+    if (addr == NIC_MMIO_TX_BD_HEAD) {
+      qemu_printf("nic_mmio_write: if0 tx_bd_head = %u\n",
+                  ((struct NICReg *)nic_reg)->tx_bd_head);
+      arg->dev = opaque;
+      arg->dst = 1;
+      arg->src = 0;
+      res = pthread_create(&thread, NULL, nic_copy_data, arg);
+      if (res != 0) {
+        qemu_printf("nic_mmio_write: pthread_create failed\n");
+      }
+      return;
+    } else if (addr == NIC_MMIO_IF_REG_SIZE + NIC_MMIO_TX_BD_HEAD) {
+      qemu_printf(
+          "nic_mmio_write: if1 tx_bd_head = %u\n",
+          ((struct NICReg *)(nic_reg + NIC_MMIO_IF_REG_SIZE))->tx_bd_head);
+      arg->dev = opaque;
+      arg->dst = 0;
+      arg->src = 1;
+      res = pthread_create(&thread, NULL, nic_copy_data, arg);
+      if (res != 0) {
+        qemu_printf("nic_mmio_write: pthread_create failed\n");
+      }
+      return;
+    }
+    break;
+  case 8:
+    *(uint64_t *)(nic_reg + addr) = val;
+    break;
+  default:
+    break;
+  }
+  qemu_printf("nic_mmio_write %lx(%u bytes) = %lx\n", addr, size, val);
 }
 
 static const MemoryRegionOps nic_mmio_ops = {
@@ -71,8 +225,8 @@ static const MemoryRegionOps nic_mmio_ops = {
     .endianness = DEVICE_LITTLE_ENDIAN,
     .impl =
         {
-            .min_access_size = 4,
-            .max_access_size = 4,
+            .min_access_size = 1,
+            .max_access_size = 8,
         },
 };
 
@@ -80,13 +234,22 @@ static void nic_pci_realize(PCIDevice *pci_dev, Error **errp) {
   NICPangoState *s = NIC_PANGO(pci_dev);
   static const uint16_t nic_pcie_offset = 0x0E0;
 
+  qemu_printf("nic_pci_realize\n");
+
   memory_region_init_io(&s->mmio, OBJECT(s), &nic_mmio_ops, s, "nic-mmio",
                         NIC_MMIO_SIZE);
   pci_register_bar(pci_dev, 0, PCI_BASE_ADDRESS_SPACE_MEMORY, &s->mmio);
 
-  if (pcie_endpoint_cap_v1_init(pci_dev, nic_pcie_offset) < 0) {
+  msi_init(pci_dev, 0, 1, true, true, errp);
+
+  if (pcie_endpoint_cap_init(pci_dev, nic_pcie_offset) < 0) {
     hw_error("Failed to initialize PCIe capability");
   }
+}
+
+static void nic_pci_uninit(PCIDevice *pci_dev) {
+  qemu_printf("nic_pci_uninit\n");
+  msi_uninit(pci_dev);
 }
 
 static void nic_class_init(ObjectClass *klass, void *data) {
@@ -94,6 +257,7 @@ static void nic_class_init(ObjectClass *klass, void *data) {
   PCIDeviceClass *k = PCI_DEVICE_CLASS(klass);
 
   k->realize = nic_pci_realize;
+  k->exit = nic_pci_uninit;
   k->vendor_id = NIC_PCI_VENDOR_ID;
   k->device_id = NIC_PCI_DEVICE_ID;
   k->revision = 0x10;
--- hw/nic/nic.h
+++ hw/nic/nic.h
@@ -4,16 +4,36 @@
 #include <stdint.h>
 #define TYPE_NIC_PANGO "nic_pango"
 #define NIC_RX_PKT_SIZE 2048
+#define NIC_TX_RING_QUEUES 16
+#define NIC_RX_RING_QUEUES 16
 
-struct nic_bd {
+#define NIC_MMIO_TX_BD_HEAD 0x00
+#define NIC_MMIO_TX_BD_TAIL 0x04
+// #define NIC_MMIO_TX_BD_HEAD_PA 0x08 // reserved
+// #define NIC_MMIO_TX_BD_TAIL_PA 0x10 // reserved
+#define NIC_MMIO_TX_BD_PA 0x18
+
+// #define NIC_MMIO_RX_BD_HEAD 0x20 // reserved
+// #define NIC_MMIO_RX_BD_TAIL 0x24 // reserved
+#define NIC_MMIO_RX_BD_HEAD_PA 0x28
+#define NIC_MMIO_RX_BD_TAIL_PA 0x30
+#define NIC_MMIO_RX_BD_PA 0x38
+
+#define NIC_MMIO_CSR_INT 0x40
+
+#define NIC_MMIO_IF_REG_SIZE 0x50
+
+
+
+struct NICBD {
   uint64_t addr;
   uint16_t len;
   uint16_t flags;
 };
 
-struct nic_rx_frame {
+struct NICRxFrame {
   uint16_t data_len;
-  uint16_t data[NIC_RX_PKT_SIZE - 2];
+  uint8_t data[NIC_RX_PKT_SIZE - 2];
 };
 
 #endif
